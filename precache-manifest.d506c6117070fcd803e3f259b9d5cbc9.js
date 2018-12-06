d920ca7e3759d8e18b96888a84be87732aa81904
mhendylazuardi <mochammad.lazuardi@students.amikom.ac.id>
2018-12-07 00:30:25 +0700

    Updates

---
+++ precache-manifest.d506c6117070fcd803e3f259b9d5cbc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "733a2940152ae43e020774c0406c00e6",
+    "url": "/pergi-com-tes/static/media/citilink.733a2940.png"
+  },
+  {
+    "revision": "61f331935072d5dd064e",
+    "url": "/pergi-com-tes/static/js/runtime~main.61f33193.js"
+  },
+  {
+    "revision": "1167113561225f83ecda",
+    "url": "/pergi-com-tes/static/js/main.11671135.chunk.js"
+  },
+  {
+    "revision": "2b3f38aa52e17e7aff9a",
+    "url": "/pergi-com-tes/static/js/1.2b3f38aa.chunk.js"
+  },
+  {
+    "revision": "1167113561225f83ecda",
+    "url": "/pergi-com-tes/static/css/main.4b894857.chunk.css"
+  },
+  {
+    "revision": "2b3f38aa52e17e7aff9a",
+    "url": "/pergi-com-tes/static/css/1.335c308e.chunk.css"
+  },
+  {
+    "revision": "8a5fb09b8e5f5db36892e3cff709fe5a",
+    "url": "/pergi-com-tes/index.html"
+  }
+];
